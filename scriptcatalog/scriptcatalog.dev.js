58587650ba1f03f480a7c67616e63cf3e42b394a
types140 <types140@fahr-zur-hoelle.org>
2020-08-26 21:02:39 +0200

    Update scriptcatalog.dev.js

--- scriptcatalog/scriptcatalog.dev.js
+++ scriptcatalog/scriptcatalog.dev.js
@@ -1,12 +1,13 @@
 (function (I18n, $) {
-    $.loadScript = function (url, callback) {
-        $.ajax({
-            url: url,
-            dataType: 'script',
-            success: callback,
-            async: true
-        });
-    }
+    //$.loadScript = function (url, callback) {
+    //    $.ajax({
+    //        url: url,
+    //        dataType: 'script',
+    //        success: callback,
+    //        async: true
+    //    });
+    //}
 
-    $.loadScript('/sites/collapseDeployments.js');
+    //$.loadScript('/sites/collapseDeployments.js');
+    console.log("Github Funktioniert");
 })(I18n, jQuery);
