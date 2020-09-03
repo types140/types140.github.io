63e9cae2489b2a414758fac6b85a90f994b511ac
Allure149 <types140@fahr-zur-hoelle.org>
2020-09-03 12:28:39 +0200

    test

--- scriptcatalog/modules/buildings.js
+++ scriptcatalog/modules/buildings.js
@@ -1,18 +1,20 @@
-Object.prototype.vehicleTable = function(){
-  try {
-    var rows = this.querySelector("tbody").querySelectorAll("tr");
-  } catch(e) {
-    return e;
-  }
+Object.defineProperty(Object.prototype, 'vehicleTable',{
+    value : function(){
+        try {
+            var rows = this.querySelector("tbody").querySelectorAll("tr");
+        } catch(e) {
+            return e;
+        }
 
-  var vehicles = [];
-  for(var i=0;i<rows.length;i++){
-      console.log(rows[i].querySelectorAll("td")[2]);
-    var attr = rows[i].querySelectorAll("td")[2].attributes.sortvalue.value;
-    var pattern = new RegExp(/(?<vehicleName>.*)\s(?<vehicleId>\d+)/g);
-    var filtered = pattern.exec(attr);
-    vehicles.push({"name":filtered[1],"id":filtered[2]});
-  }
+        var vehicles = [];
+        for(var i=0;i<rows.length;i++){
+            var attr = rows[i].querySelectorAll("td")[1].attributes.sortvalue.value;
+            var pattern = new RegExp(/(?<vehicleName>.*)\s(?<vehicleId>\d+)/g);
+            var filtered = pattern.exec(attr);
+            vehicles.push({"name":filtered[1],"id":filtered[2]});
+        }
 
-  return vehicles;
-};
+        return vehicles;
+    },
+    enumerable : false
+});
