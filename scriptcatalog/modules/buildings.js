8de8bdf249dadf702c1240a35670f83f8909d622
Allure149 <types140@fahr-zur-hoelle.org>
2020-09-03 09:41:04 +0200

    create buildings lib

---
+++ scriptcatalog/modules/buildings.js
@@ -0,0 +1,17 @@
+String.prototype.vehiclesInTable = function() {
+  try {
+    var rows = this.querySelector("tbody").querySelectorAll("tr");
+  } catch(e) {
+    return e;
+  }
+
+  var vehicles = [];
+  for(var i=0;i<rows.length;i++){
+    var attr = rows[i].querySelectorAll("td")[2].attributes.sortvalue.value;
+    var pattern = new RegExp(/(?<vehicleName>.*)\s(?<vehicleId>\d+)/g);
+    var filtered = pattern.exec(attr);
+    vehicles.push({"name":filtered[1],"id":filtered[2]});
+  }
+
+  return vehicles;
+};
