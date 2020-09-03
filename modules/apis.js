1a453b7d0c28e028bf688c09357ef75261c12d28
Allure149 <types140@fahr-zur-hoelle.org>
2020-09-03 13:20:00 +0200

    new

--- modules/apis.js
+++ modules/apis.js
@@ -1,10 +1,12 @@
 Object.defineProperty(Object.prototype, 'loadMissions',{
-    value : async function(){
-        if (!window.sessionStorage.hasOwnProperty('api_credits_cache') ||
-        JSON.parse(window.sessionStorage.api_credits_cache).lastUpdate < (new Date().getTime() - 5 * 1000 * 60))
-        await fetch('/einsaetze.json')
-            .then(res => res.json())
-            .then(data => window.sessionStorage.setItem('api_credits_cache',JSON.stringify({lastUpdate: new Date().getTime(),value: data})));
-    },
-    enumerable : false
+  value : function(){
+    if (!window.sessionStorage.hasOwnProperty('aMissions') ||
+        JSON.parse(window.sessionStorage.aMissions).lastUpdate < (new Date().getTime() - 5 * 1000 * 60))
+      fetch('/einsaetze.json')
+        .then(res => res.json())
+        .then(data => window.sessionStorage.setItem('aMissions',JSON.stringify({lastUpdate: new Date().getTime(),value: data})));
+
+    return JSON.parse(sessionStorage.aMissions).value;
+  },
+  enumerable : false
 });
